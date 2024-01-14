26d18496ea551a108f350a98d3b93768445d5689
APPLE <apple@APPLEs-MacBook-Air.local>
2024-01-14 00:28:16 -0800

    doc

--- test-output-thread/report.js
+++ test-output-thread/report.js
@@ -1,36 +1,36 @@
 $(document).ready(function() {
 CucumberHTML.timelineItems.pushArray([
   {
-    "id": "29358cd4-cba1-411c-b51c-e4721d35b528",
-    "feature": "Login page feature",
-    "scenario": "Login with correct credentials",
-    "start": 1705216475679,
-    "group": 29,
-    "content": "",
-    "tags": "",
-    "end": 1705216493531,
-    "className": "passed"
-  },
-  {
-    "id": "e1056297-d0e4-4bff-a764-310e05a9abbb",
-    "feature": "Login page feature",
-    "scenario": "Login page title",
-    "start": 1705216475670,
-    "group": 27,
-    "content": "",
-    "tags": "",
-    "end": 1705216505348,
-    "className": "passed"
-  },
-  {
-    "id": "79282be9-5b04-492e-929c-c4358dca9285",
+    "id": "64822dd7-3f53-454d-a963-58f56e0e86bb",
     "feature": "Login page feature",
     "scenario": "Forgot Password link",
-    "start": 1705216475520,
+    "start": 1705220820738,
     "group": 28,
     "content": "",
     "tags": "",
-    "end": 1705216499371,
+    "end": 1705220836521,
+    "className": "passed"
+  },
+  {
+    "id": "b2e16d3c-09eb-4193-b6a8-b2646a53d76b",
+    "feature": "Login page feature",
+    "scenario": "Login with correct credentials",
+    "start": 1705220820877,
+    "group": 29,
+    "content": "",
+    "tags": "",
+    "end": 1705220851418,
+    "className": "passed"
+  },
+  {
+    "id": "96f1419c-104c-4b0d-ae9a-e5adbb5de2d6",
+    "feature": "Login page feature",
+    "scenario": "Login page title",
+    "start": 1705220820884,
+    "group": 27,
+    "content": "",
+    "tags": "",
+    "end": 1705220830373,
     "className": "passed"
   }
 ]);
